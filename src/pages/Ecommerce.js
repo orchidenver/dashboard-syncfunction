84ed439223ca9d1b5afc0e294967b7b246e7cc36
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-03-22 19:40:16 +0200

    orders, employees & customers pages created

--- src/pages/Ecommerce.js
+++ src/pages/Ecommerce.js
@@ -8,7 +8,9 @@ import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropd
 import { useStateContext } from '../contexts/ContextProvider';
 import product9 from '../assets/product9.jpg';
 
-function Ecommerce() {
+const Ecommerce = () => {
+    const { currentColor, currentMode } = useStateContext();
+
     return (
         <div className="mt-24">
             <div className="flex flex-wrap lg:flex-nowrap justify-center ">
@@ -20,7 +22,7 @@ function Ecommerce() {
                         </div>
                         <button
                             type="button"
-                            style={{ backgroundColor: 'blue' }}
+                            style={{ backgroundColor: currentColor }}
                             className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
                         >
                             <BsCurrencyDollar />
@@ -29,7 +31,7 @@ function Ecommerce() {
                     <div className="mt-6">
                         <Button
                             color="white"
-                            bgColor={'blue'}
+                            bgColor={currentColor}
                             text="Download"
                             borderRadius="10px"
                         />
@@ -37,7 +39,7 @@ function Ecommerce() {
                 </div>
                 <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
                     {earningData.map((item) => (
-                        <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl ">
+                        <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
                             <button
                                 type="button"
                                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
@@ -56,8 +58,62 @@ function Ecommerce() {
                     ))}
                 </div>
             </div>
-        </div>
-    )
-}
 
-export default Ecommerce
+            <div className="flex gap-10 flex-wrap justify-center">
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+                    <div className="flex justify-between">
+                        <p className="font-semibold text-xl">Revenue Updates</p>
+                        <div className="flex items-center gap-4">
+                            <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                                <span>
+                                    <GoPrimitiveDot />
+                                </span>
+                                <span>Expense</span>
+                            </p>
+                            <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                                <span>
+                                    <GoPrimitiveDot />
+                                </span>
+                                <span>Budget</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-10 flex gap-10 flex-wrap justify-center">
+                        <div className=" border-r-1 border-color m-4 pr-10">
+                            <div>
+                                <p>
+                                    <span className="text-3xl font-semibold">$93,438</span>
+                                    <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                                        23%
+                                    </span>
+                                </p>
+                                <p className="text-gray-500 mt-1">Budget</p>
+                            </div>
+                            <div className="mt-8">
+                                <p className="text-3xl font-semibold">$48,487</p>
+
+                                <p className="text-gray-500 mt-1">Expense</p>
+                            </div>
+
+                            <div className="mt-5">
+                                <SparkLine currentColor={currentColor} id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} />
+                            </div>
+                            <div className="mt-10">
+                                <Button
+                                    color="white"
+                                    bgColor={currentColor}
+                                    text="Download Report"
+                                    borderRadius="10px"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <Stacked currentMode={currentMode} width="320px" height="360px" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Ecommerce;
